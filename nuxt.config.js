9533a78b9eaf9dccccc1a6c0bd89f67549a975cb
Po Rith <poamrong@rith.com>
2019-09-04 17:30:33 -0700

    make static mock

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,21 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'xCompTrainer | Track Your History in Crucible',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png?v=3' },
+      { rel: "stylesheet", type: "text/css", href: "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" }
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js' },
+      { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
+      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js' }
+    ]
   },
 
   /*
@@ -59,7 +67,7 @@ module.exports = {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          // loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
       }
--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,123 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        xcomptrainer
-      </h1>
-      <h2 class="subtitle">
-        My super-excellent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+  <div>
+    <div class="container">
+      <div class="row pb-3">
+        <div class="col-md-6">
+          xCompTrainer <br>
+          jPoechill
+        </div>
+        <div class="col-md-6 text-right">glory (0): 0</div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <hr class="bg-white">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 text-white text-center">
+          <a href="">Tables</a> | <a href="">Graphs</a>
+        </div>
       </div>
     </div>
-  </section>
+
+    <div class="container pt-2">
+      <div v-for="n in 4" :key="n" class="row">
+        <div class="col-md-4 my-3">
+          <div class="tab bg-dark ripple-drk noselect text-center text-uppercase p-2 rounded font-weight-bold">
+            Loss
+          </div>
+          <div class="w-100">
+            <div class="d-inline-block text-white float-right">
+              + 
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 my-3">
+          <div class="tab bg-light text-dark noselect ripple-lgt text-center text-uppercase p-2 rounded font-weight-bold">
+            Win
+          </div>
+          <div class="w-100">
+            <div class="d-inline-block text-white float-right">
+              + 
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 my-3">
+          <div class="tab bg-dark ripple-drk noselect text-center text-uppercase p-2 rounded font-weight-bold">
+            Loss
+          </div>
+          <div class="w-100">
+            <div class="d-inline-block text-white float-right">
+              + 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          Discretion: <br>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita tenetur commodi ipsum, non impedit iste molestias veniam quaerat ab accusamus voluptate sit quos illo dolorum?
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+a {
+  color: #ffffff;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.tab {
+  box-shadow: 1px 1px 1px 1px hsla(0, 0%, 6%, .3);
+  transition: all .3s ease-out;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.tab:hover {
+  box-shadow: 3px 3px 5px 3px rgba(16, 16, 16, .3);
+  /* transition: .2s ease-in-out; */
+  cursor: pointer;
 }
 
-.links {
-  padding-top: 15px;
+/* Ripple effect */
+.ripple-lgt, .ripple-drk {
+  background-position: center;
+  /* transition: background 0.8s; */
+}
+
+.ripple-lgt:hover {
+  background: #eee radial-gradient(circle, transparent 1%, #f1f1f1 1%) center/15000%;
+}
+
+.ripple-lgt:active {
+  background-size: 100%;
+  transition: background 0s;
+}
+
+.ripple-drk:hover {
+  background: #555 radial-gradient(circle, transparent 1%, #333 1%) center/15000%;
+}
+
+.ripple-drk:active {
+  background-size: 100%;
+  transition: background 0s;
+}
+
+/* Disable Text Selection */
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
 }
 </style>
